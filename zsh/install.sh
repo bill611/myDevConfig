59ea705a9a84cce5b0ab074ca2f7579909420602
bill611 <bill4regist@163.com>
2016-06-02 11:19:19 +0800

    修改zsh install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,7 +2,7 @@
 
 TIME=`date +%Y%m%d_%H_%M_%S`
 
-tmux --help > /dev/null 2>&1
+zsh --help > /dev/null 2>&1
 if [ ! $? = 0 ] ; then
 	echo "zsh didn't install"
 	echo "install zsh "
@@ -13,21 +13,3 @@ if [ ! $? = 0 ] ; then
 	chsh -s /bin/zsh
 fi
 
-if [ -e ~/.tmux.conf ] ;then
-	mkdir -p ~/.tmux_history
-	echo "mv ~/.tmux ~/.tmux_history/tmux_bak_$TIME"
-	mv ~/.tmux.conf ~/.tmux_history/tmux_bak_$TIME
-fi
-
-if [ -e tmux.conf ] ; then
-	echo "cp tmux.conf ~/.tmux.conf"
-	cp tmux.conf ~/.tmux.conf
-else
-	echo "tmux.conf didn't exist!"
-	exit 1
-fi
-
-if [ -e clean_history.sh ] ; then 
-	echo "cp clean_history.sh ~/.tmux_history/ "
-	cp clean_history.sh ~/.tmux_history/
-fi
